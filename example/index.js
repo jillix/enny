050a7a1dd49cd9117d9013656448537e69df6122
Ionică Bizău <bizauionica@gmail.com>
2015-07-20 10:50:07 +0300

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,13 @@
+// Dependencies
+var Enny = require("../lib");
+
+// Initialize Enny in memory
+var e = new Enny();
+
+// Add two instances
+var aIns = e.addInstance({ name: "A" })
+  , bIns = e.addInstance({ name: "B" })
+  ;
+
+// Connect A -> B
+aIns.connect(bIns);
